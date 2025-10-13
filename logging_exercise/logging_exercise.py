38578654b91ab4382714db51e48a27114fccd8c1
Elmar Maas <gqq77sk@vwdewobllu00417.cariad.vwgroup.com>
2025-10-13 15:11:20 +0200

    improved exercise to pylint score 10

--- logging_exercise/logging_exercise.py
+++ logging_exercise/logging_exercise.py
@@ -1,3 +1,5 @@
+""" This is exercise 4.19 from Udacity Training Machine Learning DevOps Engineer Nanodegree.
+The goal of this exercise is to practice logging and exception handling."""
 ## STEPS TO COMPLETE ##
 # 1. import logging
 # 2. set up config file for logging called `results.log`
@@ -12,28 +14,36 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def config_logger():
+    """Configures logging to log to a file named results.log"""
     logging.basicConfig(filename="./results.log", level=logging.INFO)
-    
+
+
 def sum_vals(a, b):
-    '''
+    """Returns the sum of a and b including some error checking
+    
     Args:
         a: (int)
         b: (int)
     Return:
         a + b (int)
-    '''
+    """
+
     error = False
-    if not (isinstance(a, (int, float))):
-        logging.error(f"Parameter a is not a number: {a}.")
+    if not isinstance(a, (int, float)):
+        logging.error("Parameter a is not a number: %s.", a)
         error = True
-    if not (isinstance(b, (int, float))):
-        logging.error(f"Parameter b is not a number: {b}.")
+    if not isinstance(b, (int, float)):
+        logging.error("Parameter b is not a number: %s.", b)
         error = True
-    if (error == False):
-        c = a+b
-        logging.info(f"SUCCESS: Computed a+b = {c}.")
+    if error is False:
+        c = a + b
+        logging.info("SUCCESS: Computed a+b = %s.", c)
         return c
+    #if we get here, there was an error
+    return "a + b cannot be computed"
+
 
 if __name__ == "__main__":
     config_logger()
