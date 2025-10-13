7df30f0f4023c6601d11d11400e2996932dce4cb
Elmar Maas <elmar.maas@cariad.technology>
2025-10-13 17:14:41 +0200

    worked on exercise

--- logging_exercise/testing_logging.py
+++ logging_exercise/testing_logging.py
@@ -1,3 +1,6 @@
+
+""" Exercise 4.22 from Udacity Training Machine Learning DevOps Engineer Nanodegree.
+The goal of this exercise is to practice logging and exception handling."""
 ## STEPS TO COMPLETE ##
 # 1. import logging
 # 2. set up config file for logging called `test_results.log`
@@ -11,6 +14,16 @@
 # 5. use pylint and autopep8 to make changes
 #    and receive 10/10 pep8 score
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def config_logger():
+    """Configures logging to log to a file named test_results.log"""
+    logging.basicConfig(filename="./test_results.log", level=logging.INFO)
+
+
 def divide_vals(numerator, denominator):
     '''
     Args:
@@ -21,9 +34,12 @@ def divide_vals(numerator, denominator):
         fraction_val: (float) numerator/denominator
     '''
     try:
-        fraction_val = numerator/denominator
+        fraction_val = numerator / denominator
+        logger.info("SUCCESS: Computed numerator/denominator = %s.",
+                    fraction_val)
         return fraction_val
     except ZeroDivisionError:
+        logger.error("Denominator cannot be zero: %s.", denominator)
         return "denominator cannot be zero"
 
 
@@ -36,12 +52,16 @@ def num_words(text):
         num_words: (int) number of words in the string
     '''
     try:
-        num_words = len(text.split())
-        return num_words
+        words = len(text.split())
+        logger.info("SUCCESS: Computed number of words = %s.", words)
+        return words
     except AttributeError:
+        logger.error("Text argument must be a string: %s.", text)
         return "text argument must be a string"
 
+
 if __name__ == "__main__":
+    config_logger()
     divide_vals(3.4, 0)
     divide_vals(4.5, 2.7)
     divide_vals(-3.8, 2.1)
